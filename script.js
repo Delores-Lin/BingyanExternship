1ced20364dcaa2bb0950ed94c87d55b1529139fe
DeloresLin <linzhibo6@gmail.com>
2024-10-05 02:12:53 +0800

    10.4

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+
+// 实现最近对话的收起和展开
 const conversation = document.querySelector(".recentConversation");
 
 const hideConversation = document.querySelector(".allConversation");
 
 const hideArraw = document.querySelector(".arraw");
+
 conversation.addEventListener("click", function () {
     if (hideConversation.style.display === "flex") {
         hideConversation.style.display = "none";
@@ -10,5 +13,28 @@ conversation.addEventListener("click", function () {
     }else {
         hideConversation.style.display = "flex";
         hideArraw.src = "photos/arrawDown.png";
+    }
+})
+
+//实现资助计划的删除
+const deleteAd = document.querySelector(".cancel");
+
+const ad = document.querySelector(".ad");
+
+deleteAd.addEventListener("click", function () {
+    ad.style.display = "none";
+})
+    
+//发送按钮样式变化
+const send = document.querySelector(".send");
+
+const input = document.querySelector(".input");
+
+input.addEventListener("input", function () {
+    if (input.value.trim() !== "") {
+        send.disabled = false;
+        send.add("enabled");
+    } else {
+        send.disabled = true;
     }
 })
