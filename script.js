2c2a753ca9f3255353271229db688771e822b02d
DeloresLin <linzhibo6@gmail.com>
2024-10-06 22:24:13 +0800

    10.6

--- script.js
+++ script.js
@@ -6,13 +6,17 @@ const hideConversation = document.querySelector(".allConversation");
 
 const hideArraw = document.querySelector(".arraw");
 
+const menu1 = document.querySelector(".menu1 div");
+
 conversation.addEventListener("click", function () {
     if (hideConversation.style.display === "flex") {
         hideConversation.style.display = "none";
         hideArraw.src = "photos/arrawRight.png";
+        menu1.style.display = "block";
     }else {
         hideConversation.style.display = "flex";
         hideArraw.src = "photos/arrawDown.png";
+        menu1.style.display = "none";
     }
 })
 
@@ -38,3 +42,125 @@ input.addEventListener("input", function () {
         send.disabled = true;
     }
 })
+
+// 登录与注册页面的打开与关闭
+const openLogin = document.querySelector(".userName");
+
+const loginAndSignup = document.querySelector(".loginAndSignup");
+
+const closeLogin = document.querySelector(".closeLogin");
+const closeSignup = document.querySelector(".closeSignup");
+
+closeLogin.addEventListener("click", function () {
+    loginAndSignup.style.display = "none";
+})
+closeSignup.addEventListener("click", function () {
+    loginAndSignup.style.display = "none";
+})
+
+openLogin.addEventListener("click", function () {
+    loginAndSignup.style.display = "flex";
+})
+
+//按钮的点击效果
+
+//注册登录界面切换
+const login = document.querySelector(".login");
+const signup = document.querySelector(".signup");
+
+const switchSignup = document.querySelector("#signup");
+const switchLogin = document.querySelector("#login");
+
+switchSignup.addEventListener("click", function () {
+    login.style.display = "none";
+    signup.style.display = "flex";
+})
+switchLogin.addEventListener("click", function () {
+    login.style.display = "flex";
+    signup.style.display = "none";
+})
+
+
+//登录注册邮箱验证
+const checkmail = function (email,error) {
+    const inputMail = document.querySelector(email);
+    const theError = document.querySelector(error);
+    inputMail.onblur = function () {
+        if (!inputMail.value.includes("@")) {
+            inputMail.classList.add("invalid");
+            inputMail.style.border = "1px solid red";
+            theError.innerHTML = "*请输入正确的邮箱";
+        }
+    };
+    inputMail.onfocus = function () {
+        if (this.classList.contains("invalid")) {
+            this.style.border = "none";
+            inputMail.classList.remove("invalid");
+            theError.innerHTML = "";
+        }
+    };
+}
+checkmail(".loginEmail","#loginError");
+checkmail(".signupEmail", "#signupError");
+
+//登录与注册按钮验证
+const checkButton = function (selectButton,selectEmail,selectPassword) {
+    const button = document.querySelector(selectButton);
+    const email = document.querySelector(selectEmail);
+    const password = document.querySelector(selectPassword);
+    password.addEventListener("input", function () {
+    if (email.value.includes("@") && password.value.trim() !== "") {
+        button.disabled = false;
+        button.add("enabled");
+    } else {
+        button.disabled = true;
+        }
+        });
+}
+checkButton(".loginBtn", ".loginEmail", ".loginPassword");
+checkButton(".signupBtn", ".signupEmail", ".signupPassword");
+
+//实现注册
+const signupBtn = document.querySelector(".signupBtn");
+signupBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    const username = document.querySelector(".username").value;
+    const signupEmail = document.querySelector(".signupEmail").value;
+    const signupPassword = document.querySelector(".signupPassword").value;
+    const getUsers = JSON.parse(localStorage.getItem("users")) || [];
+    const user = getUsers.find(user => user.email === signupEmail);
+    if (user) {
+        alert("该邮箱已被注册!");
+        return;
+    }else{
+    const newUser = {
+        username: username,
+        email: signupEmail,
+        password: signupPassword
+    };
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+    users.push(newUser);
+    localStorage.setItem("users", JSON.stringify(users));
+    alert("注册成功!");
+        window.location.href = "fakeAI.html";
+    }
+});
+//实现登录
+const loginBtn = document.querySelector(".loginBtn");
+loginBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+const getUsers = JSON.parse(localStorage.getItem("users")) || [];
+const loginEmail = document.querySelector(".loginEmail").value;
+    const loginPassword = document.querySelector(".loginPassword").value;
+    const wrong = document.querySelector(".wrong");
+    const userName = document.querySelector(".userName");
+    const loginAndSignup = document.querySelector(".loginAndSignup");
+const user = getUsers.find(user => user.email === loginEmail && user.password === loginPassword);
+if (user) {
+    alert("登录成功!");
+    userName.innerHTML = user.username;
+    loginAndSignup.style.display = "none";
+} else {
+    wrong.innerHTML = "*邮箱或密码错误";
+    }
+});
